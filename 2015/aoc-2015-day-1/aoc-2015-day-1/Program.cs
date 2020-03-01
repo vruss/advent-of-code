e3b00fa1c098fc3cd0c231e45d3891588c73df65
Viktor Rosvall <virosen@yahoo.se>
2020-03-01 16:54:37 +0100

    Part 1 complete

---
+++ 2015/aoc-2015-day-1/aoc-2015-day-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace aoc_2015_day_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"input.txt";
+            string inputString;
+            int floor = 0;
+
+            // Open file and read content
+            using (StreamReader streamReader = File.OpenText(path))
+            {
+                inputString = streamReader.ReadToEnd();
+            }
+
+            // Traverse the apartment building
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] == '(')
+                {
+                    floor++;
+                }
+                else if (inputString[i] == ')')
+                {
+                    floor--;
+                }
+            }
+
+            // Write resulting floor
+            Console.WriteLine(floor);
+        }
+    }
+}
---
+++ 2015/aoc-2015-day-1/aoc-2015-day-1/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("aoc_2015_day_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("aoc_2015_day_1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("567F028F-1116-4421-9200-F43FAF1D5162")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
