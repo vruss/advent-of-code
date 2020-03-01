f8dc7694a7bcd269584b03ce3e5627999eac203f
Viktor Rosvall <virosen@yahoo.se>
2020-03-01 17:13:04 +0100

    Refactored into a new solution

---
+++ 2015/aoc-2015/day-1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace day_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"input.txt";
+            string inputString;
+            int floor = 0;
+
+            // Open file and read content
+            using (StreamReader streamReader = File.OpenText(path))
+            {
+                inputString = streamReader.ReadToEnd();
+            }
+
+            // Traverse the apartment building
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] == '(')
+                {
+                    floor++;
+                }
+                else if (inputString[i] == ')')
+                {
+                    floor--;
+                }
+
+                // (PART 2) Check if santa is at the basement for the first time
+                if (floor == -1)
+                {
+                    Console.WriteLine("Santa first entered basement at position {0}", ++i);
+                    break;
+                }
+            }
+
+            // Write resulting floor
+            Console.WriteLine("Santa exited the apartment at floor {0}", floor);
+        }
+    }
+}
